a34c33e4c87ed97094caaba0092b26bfbe2bfbb6
zhoudong666 <936048983@qq.com>
2021-08-23 23:08:31 +0800

    使用svg-spite-laoder对icon文件夹里的svg文件处理,svg图标文件使用svgo精简,以及基本使用

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './icons/svg/friend-fields.svg'
+import './icons/svg/search.svg'
+import './icons/svg/save.svg'
+
 Vue.config.productionTip = false
 
 new Vue({
